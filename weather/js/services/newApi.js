9c2eddaa2accff6ac8054b50a02440750b2111a7
bjorneboman <gostabohman@gmail.com>
2025-12-08 15:22:35 +0100

    Fixes fetch requests to the new API (newAPI.js needs to be referred to instead of oldapi.js)

--- weather/js/services/newApi.js
+++ weather/js/services/newApi.js
@@ -6,14 +6,16 @@ export async function getCity(string){
         const response = await fetch(url);
         if(!response.ok) console.log("faulty request");
         const data = await response.json();
-        for(let i = 0; i < data.length; i++){
-            if(data[i].name.toLowerCase().trim() == string){
-                return {name:data[i].name, lat: Math.round((data[i].longitude)*10000)/10000, lon: Math.round((data[i].latitude) * 10000)/10000};
-            }
-        }
-        // const city = data.filter((city) => city.name.toLowerCase().trim() == string)
-        // console.log(city);
-        // return {lat: city[0].latitude, lon: city[0].longitude}
+
+        // console.log(data);
+
+        const city = data.filter((city) => city.name.toLowerCase().trim() === string)
+
+        // console.log("newApi.js => city: ", city[0]);
+        
+        return {results: [{name: city[0].name, admin1: city[0].name, country: "Sweden", latitude: city[0].latitude, longitude: city[0].longitude}]}
+         
+
     }catch(error){
         console.log("error: ", error.message);
     }
@@ -28,7 +30,6 @@ export async function getWeather(lat, lon){
         const response = await fetch(url);
         if(!response.ok) console.log("faulty request");
         const data = await response.json();
-        console.log(data);
         return data
     }catch(error){
         console.log(error.message);
