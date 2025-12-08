834b5b291d3cc0bc4dcfcc52ae484b6c5d2e7133
isabelle.amsalu@gmail.com <isabelle.amsalu@gmail.com>
2025-12-08 11:05:39 +0100

    search on enter and click, clear input field

--- weather/js/main.js
+++ weather/js/main.js
@@ -13,7 +13,7 @@ const forecastEl = document.querySelector("#forecast");
 const historyEl = document.querySelector("#history");
 const textInputEl = document.querySelector("input");
 const inputWrapperEl = document.querySelector(".input-wrapper");
-/* const sendButtonEl = document.querySelector("#send-input"); */
+const sendButtonEl = document.querySelector("#send-input");
 
 let historyList = new History(historyEl);
 
@@ -26,18 +26,42 @@ textInputEl.addEventListener("input", async () => {
   // Index kopierar key och skickas till city-instansieringen.
   dropDown.element.addEventListener("click", async (event) => {
     let index = findIndexOfDropItem(event);
-    const city = new City(handleText(textInputEl.value), index);
-    await city.fetchCity();
-
-    city.buildMainWeather(mainWeatherEl);
-    city.buildForecast(forecastEl);
-    historyList.cityListAdd(city);
+    await runSearch(index);
 
     dropDown.element.remove();
   });
 
   inputWrapperEl.insertBefore(dropDown.element, inputWrapperEl.firstChild);
 });
+
+textInputEl.addEventListener("keydown", async (e) => {
+  if (e.key === "Enter") {
+    e.preventDefault();
+
+    //default to first search result
+    await runSearch(0);
+
+    textInputEl.value = "";
+  }
+});
+
+sendButtonEl.addEventListener("click", async (e) => {
+  e.preventDefault();
+  await runSearch(0);
+
+  textInputEl.value = "";
+});
+
+async function runSearch(index) {
+  if (!textInputEl.value.trim()) return;
+
+  const city = new City(handleText(textInputEl.value), index);
+  await city.fetchCity();
+
+  city.buildMainWeather(mainWeatherEl);
+  city.buildForecast(forecastEl);
+  historyList.cityListAdd(city);
+}
 
 function findIndexOfDropItem(event) {
   const children = [...event.target.parentElement.children];
