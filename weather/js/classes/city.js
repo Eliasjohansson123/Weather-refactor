a5415fd72da3142933e9592da4564f753cafe237
Alyanna Karlsson <aok.karlsson@gmail.com>
2025-12-09 14:06:37 +0100

    formated date in forecast

--- weather/js/classes/city.js
+++ weather/js/classes/city.js
@@ -1,55 +1,61 @@
-import { getCity, getWeather } from "../services/oldapi.js";
-import { weatherEmojis } from "../services/weathercodes.js";
+import { getCity, getWeather } from '../services/oldapi.js';
+import { weatherEmojis } from '../services/weathercodes.js';
 
 export class City {
+  // cityName är stadsnamnet sim skickas in till getCity API-call, CityIndex är vilket index i City som vi vill ha
+  // (specifik stad som matchar getCity()-kallningen), RequestWeather är en boolisk variabel som bestämmer om instansen
+  // skall innehålla väderinformation.
 
-    // cityName är stadsnamnet sim skickas in till getCity API-call, CityIndex är vilket index i City som vi vill ha
-    // (specifik stad som matchar getCity()-kallningen), RequestWeather är en boolisk variabel som bestämmer om instansen
-    // skall innehålla väderinformation.
+  // parent parametern i build-funktionerna skall skickas som en DOM-Nod.
 
-    // parent parametern i build-funktionerna skall skickas som en DOM-Nod.
+  constructor(cityName, cityIndex) {
+    this.cityName = cityName;
+    this.cityIndex = cityIndex;
+  }
+  async fetchCity() {
+    const city = await getCity(this.cityName);
+    console.log('fetching city...', city);
+    this.fetchedCity = city.results[this.cityIndex];
 
-    constructor(cityName, cityIndex){
-        this.cityName = cityName;
-        this.cityIndex = cityIndex;
-    }
-    async fetchCity(){
+    this.cityName = this.fetchedCity.name;
+    this.lat = city.results[this.cityIndex].latitude;
+    this.lon = city.results[this.cityIndex].longitude;
 
-        const city = await getCity(this.cityName);
-        console.log("fetching city...", city);
-        this.fetchedCity = city.results[this.cityIndex];
+    const weather = await getWeather(this.lat, this.lon);
+    this.weatherNow = weather.current;
+    this.futureWeather = weather.daily;
+  }
+  buildForecast(parent) {
+    parent.innerHTML = '';
+    for (let i = 1; i < 7; i++) {
+      const cont = document.createElement('div');
+      cont.classList.add('forecast-box');
+      const forecastText = document.createElement('p');
+      forecastText.classList.add('forecast-text');
 
-        this.cityName = this.fetchedCity.name;
-        this.lat = city.results[this.cityIndex].latitude;
-        this.lon = city.results[this.cityIndex].longitude;
+      const dateString = this.futureWeather.time[i];
+      const date = new Date(dateString);
+      const formattedDate = date.toLocaleDateString('en-US', {
+        weekday: 'short',
+        month: 'short',
+        day: 'numeric',
+      });
+      
 
-        const weather = await getWeather(this.lat, this.lon);
-        this.weatherNow = weather.current;
-        this.futureWeather = weather.daily;
-    }     
-    buildForecast(parent){
-        parent.innerHTML = "";
-        for(let i = 1; i < 7;i++){
-            const cont = document.createElement('div');
-            cont.classList.add("forecast-box");
-            const forecastText = document.createElement("p");
-            forecastText.classList.add("forecast-text");
-            
-            forecastText.textContent = `
-            ${this.futureWeather.time[i]}
-            temperatures: 
-            ${this.futureWeather.temperature_2m_max[i]}
-            - 
-            ${this.futureWeather.temperature_2m_min[i]}
+
+      forecastText.textContent = `
+            ${formattedDate}
+            high: ${this.futureWeather.temperature_2m_max[i]}
+            low: ${this.futureWeather.temperature_2m_min[i]}
             ${weatherEmojis[this.futureWeather.weather_code[i]]}
             `;
-            cont.appendChild(forecastText);
-            parent.appendChild(cont);
-        }
-        // DOM-Manip
+      cont.appendChild(forecastText);
+      parent.appendChild(cont);
     }
-    buildMainWeather(parent){
-        parent.innerHTML = `
+    // DOM-Manip
+  }
+  buildMainWeather(parent) {
+    parent.innerHTML = `
         <h1>${this.fetchedCity.name}</h1>
         <h2>${this.fetchedCity.country}, ${this.fetchedCity.admin1}</h2>
         <article>
@@ -61,33 +67,29 @@ export class City {
             </span>
         </article>    
         `;
-        // DOM-Manip
-    }
-    buildHistory(parent){
+    // DOM-Manip
+  }
+  buildHistory(parent) {
+    const historyCard = document.createElement('article');
+    const button = document.createElement('button');
+    const h3 = document.createElement('h3');
+    const p_1 = document.createElement('p');
+    const p_2 = document.createElement('p');
 
-        const historyCard = document.createElement('article');
-        const button = document.createElement('button');
-        const h3 = document.createElement('h3');
-        const p_1 = document.createElement('p');
-        const p_2 = document.createElement('p');
+    button.addEventListener('click', this.removeHistory);
 
-        button.addEventListener("click", this.removeHistory);
+    historyCard.classList.add('history-card');
+    button.classList.add('delete-history');
 
-        historyCard.classList.add('history-card');
-        button.classList.add('delete-history');
+    button.textContent = 'X';
+    h3.textContent = `${this.fetchedCity.name}`;
+    p_1.textContent = `${this.fetchedCity.country}`;
+    p_2.textContent = `${this.weatherNow.temperature_2m}`;
 
-        button.textContent = "X";
-        h3.textContent = `${this.fetchedCity.name}`;
-        p_1.textContent = `${this.fetchedCity.country}`;
-        p_2.textContent = `${this.weatherNow.temperature_2m}`;
-        
-        historyCard.append(button, h3, p_1, p_2);
-        parent.append(historyCard);
-        //parent.insertBefore(historyCard, parent.firstChild);
-    }
-    
-    removeHistory(cityList, target){
-        
-    }
+    historyCard.append(button, h3, p_1, p_2);
+    parent.append(historyCard);
+    //parent.insertBefore(historyCard, parent.firstChild);
+  }
 
+  removeHistory(cityList, target) {}
 }
