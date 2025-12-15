adcca7610460dd458cd42abbf0b672b84321bf7e
isabelle.amsalu@gmail.com <isabelle.amsalu@gmail.com>
2025-12-15 15:35:40 +0100

    docs: historyListHandler.js jsdocs

--- weather/js/functions/HistoryList/historyListHandler.js
+++ weather/js/functions/HistoryList/historyListHandler.js
@@ -1,25 +1,31 @@
 import { historyListBuilder } from './historyListBuilder.js';
 
-// kollar på listan med cities och kör byggfuntionen i city om
-// den godtas till historiken.
-
-// DOMParent skickas som en DOM-nod som historyCard appendas till. cities är en lista med City-instanser,
-// city är den nyaste instansen av City.
+/**
+ * Handles updates to the citry history list
+ * makes sure there are no duplicates, list size limit andreloads UI
+ * @param {HTMLElement} DOMParent - history card container
+ * @param {City[]} cities - array with stored City instances
+ * @param {City} city - new city instance
+ */
 
 export function historyListHandler(DOMParent, cities, city) {
+  // ignore city if it is already in history (based on coords)
   if (
     cities.some((c) => c.lat === city.lat) &&
     cities.some((c) => c.lon === city.lon)
   ) {
     console.log('already existing check', cities);
+
+    // Limit history to 5 cities
   } else if (cities.length >= 5) {
     cities.pop();
     cities.unshift(city);
     historyListBuilder(DOMParent);
-    // historyListBuilder(DOMParent, cities);
   } else {
+    // Add the newest city to beginnin of list
     cities.unshift(city);
+
+    //clear history container before rendering cards again
     historyListBuilder(DOMParent);
-    // historyListBuilder(DOMParent, cities);
   }
 }
