e69a9cd87b2bb8842cac4d166d2e4600a8b882c7
isabelle.amsalu@gmail.com <isabelle.amsalu@gmail.com>
2025-12-16 11:22:09 +0100

    docs: jsdocs comments for build.js

--- weather/js/classes/build.js
+++ weather/js/classes/build.js
@@ -1,14 +1,25 @@
+/**
+ * Handles building and updateing weahter-related content
+ *  */
+
 export class Build {
+  /**
+   * Creates a build instance from a City object
+   *
+   * @param {Object} city - City instance containing fetched city and weather data
+   */
+
   constructor(city) {
     this.city = city.fetchedCity;
     this.weatherNow = city.weatherNow;
     this.futureWeather = city.futureWeather;
   }
-  // Skicka parent som en DOM-Nod
-  buildHistory(parent) {
-    // DOM-Manip
-  }
-  buildMainWeather(parent, stad) {
+
+  /**
+   * Builds and displays the main weather information for selected city
+   */
+
+  buildMainWeather(parent) {
     parent.innerHTML = `
         <h1>${this.city.name}</h1>
         <h2>${this.city.country}, ${this.city.admin1}</h2>
@@ -21,8 +32,12 @@ export class Build {
             </span>
         </article>    
         `;
-    // DOM-Manip
   }
+
+  /**
+   * Builds and displays forecast
+   * @param {HTMLElement} parent  - parent element where forecast is rendered
+   */
   buildForecast(parent) {
     parent.innerHTML = '';
     for (let i = 1; i < 7; i++) {
@@ -41,7 +56,13 @@ export class Build {
       cont.appendChild(forecastText);
       parent.appendChild(cont);
     }
-    // DOM-Manip
+
+    /**
+     * Builds all weahter related sections at once
+     * @param {HTMLElement} historyParent - Parent element for search histyry
+     * @param {HTMLElement} mainParent - Parent element for main weather display
+     * @param {HTMLElement} forecastParent - Parent element for forecast display
+     */
   }
   buildAll(historyParent, mainParent, forecastParent) {
     this.buildHistory(historyParent);
