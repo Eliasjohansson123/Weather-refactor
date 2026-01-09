983b3b5d30ec18e24041e6210fd0fa9d1c555826
isabelle.amsalu@gmail.com <isabelle.amsalu@gmail.com>
2026-01-09 10:54:52 +0100

    remove unused imports from main.js

--- weather/js/main.js
+++ weather/js/main.js
@@ -5,13 +5,9 @@
 
 import { City } from './classes/city.js';
 import { handleText } from './services/inputs.js';
-import { historyListHandler } from './functions/HistoryList/historyListHandler.js';
-import { getCity, getWeather } from './services/oldapi.js';
 import { makeDropDown, debounce } from './functions/dropDown/dropdown.js';
-import { Build } from './classes/build.js';
 import { History } from './classes/History.js';
 //import { getCity, getWeather } from "./services/newApi.js";
-import { getUserLocation } from './functions/location/userLocation.js';
 
 const mainWeatherEl = document.querySelector('#main-weather');
 const forecastEl = document.querySelector('#forecast');
